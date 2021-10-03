9d26b26b77e0ed2849750f6ec63825dac293ccaa
Evgeny-Samoshin <samevgeny@gmail.com>
2021-10-03 10:06:02 +0400

    add lists layout

--- src/App.js
+++ src/App.js
@@ -1,37 +1,70 @@
 import './App.scss';
-import searchIcon from './assets/icons/search.png'
+import searchIcon from './assets/icons/search.svg';
+import bellIcon from './assets/icons/bell.svg';
+import avatar from './assets/img/avatar1.svg';
+import female1 from './assets/img/female1.svg';
+import male1 from './assets/img/male1.svg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="app-header">
         <form className="search-form">
           <label><img src={searchIcon} alt="icon"></img></label>
           <input type="text" placeholder="Search for any training you want"></input>
         </form>
-        <div className="notification">icon-notification</div>
-        <div className="profile">profile-icon</div>
+        <div className="notification"><img src={bellIcon} alt="bell-icon"></img></div>
+        <div className="profile"><img src={avatar} alt="avatar"></img></div>
       </header>
       <main>
         <div className="head-block">
-          <h1>You’ve got 7 task today </h1>
-          <button><i>+</i>Add new</button>
+          <h1>You’ve got <span>7 task</span> today </h1>
+          <button><span>+</span>Add new</button>
         </div>
         <div className="main-block">
           <div className="on-hold">
-            <h2>on-hold</h2>
+            <h2>On Hold</h2>
             <ul className="task-list">
-              <li></li>
-            </ul>
-          </div>
-          <div className="compleated">
-            <h2>compleated</h2>
-            <ul className="task-list">
-              <li>
+              <li className="task-list__item">
                 <span>Evaluate the addition and deletion of user IDs.</span>
                 <div className="progress-status">In Progress</div>
                 <div className="priority">Normal</div>
-                <div className="team"></div>
+                <ul className="team-list">
+                  <li className="team-list__item">
+                    <img src={female1} alt="teammate"></img>
+                  </li>
+                </ul>
+                <div className="options"></div>
+              </li>
+              <li className="task-list__item">
+                <span>Evaluate the addition and deletion of user IDs.</span>
+                <div className="progress-status">In Progress</div>
+                <div className="priority">Normal</div>
+                <ul className="team-list">
+                  <li className="team-list__item">
+                    <img src={female1} alt="teammate"></img>
+                  </li>
+                </ul>
+                <div className="options"></div>
+              </li>
+            </ul>
+          </div>
+          <div className="compleated">
+            <h2>Compleated</h2>
+            <ul className="task-list">
+              <li className="task-list__item">
+                <span>Evaluate the addition and deletion of user IDs.</span>
+                <div className="progress-status">In Progress</div>
+                <div className="priority">Normal</div>
+                <ul className="team-list">
+                  <li className="team-list__item">
+                    <img src={female1} alt="teammate"></img>
+                  </li>
+                  <li className="team-list__item">
+                    <img src={male1} alt="teammate"></img>
+                  </li>
+                </ul>
                 <div className="options"></div>
               </li>
             </ul>
